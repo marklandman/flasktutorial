82c131144af11bc8603f0d71263f06dc0f86968e
Mark <marklandman@yahoo.com>
2015-02-24 11:32:27 -0500

    completed part 3 of the megatutorial

--- views.py
+++ views.py
@@ -32,4 +32,11 @@ def index():
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
-    return render_template('login.html',title='Sign In',form=form)
+    if form.validate_on_submit():
+        flash('Login requested for OpenID={}, remember_me={}'.format(
+            form.openid.data,str(form.remember_me.data)))
+        return redirect('/index')
+    return render_template('login.html',
+                           title='Sign In',
+                           form=form,
+                           providers=app.config['OPENID_PROVIDERS'])
